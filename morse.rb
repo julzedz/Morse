b274783653f474ed64646c8d1517ea1c27f5fcee
Fredo St Fleur <fredostfleur23@gmail.com>
2023-06-15 13:41:45 -0400

    - Add decode_message method

--- morse.rb
+++ morse.rb
@@ -14,3 +14,10 @@ ALPHABET = {
     word.split.each { |i| str += decode_char(i) }
     str
   end
+
+  def decode_message(morse_message)
+    message_array = morse_message.split('   ')
+    message = ''
+    message_array.each { |i| message += "#{decode_word(i)} " }
+    message
+  end
